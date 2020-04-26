032089f74d0252b4cdd74ed43e46a4a0741cde24
rizki001 <milky_mw001@yahoo.com>
2020-04-26 22:03:22 +0800

    Dual Input String kedalam Variable

--- InputOutput.java
+++ InputOutput.java
@@ -4,5 +4,10 @@ public class InputOutput{
  public static void main(String [] args){
   Scanner scanner = new Scanner(System.in);
 
+  String nama;
+  System.out.print("Masukkan nama anda : ");
+  nama = scanner.nextLine();
+
+
  }
 }
